6216c7e8926a54e4b6375f75aa04d5cfa3417b51
Endurance, the Martian <afriviral@gmail.com>
2023-07-25 16:52:08 +0100

    changes/ base url --prod

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 const config = {
     api: {
-        // baseUrl: 'https://www.api.blockchainstakingpro.com/api'
-        baseUrl: 'http://localhost:8000/api'
+        baseUrl: 'https://www.api.blockchainstakingpro.com/api'
     },
 };
 
